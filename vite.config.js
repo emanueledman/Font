390ad29775ff2330a937419435c3a013cb4f5386
Emanuel Joao <emanuelhedima@gmail.com>
2025-04-12 16:24:16 +0100

    gfhg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/'
+    base: '/', // Garante que os caminhos sejam relativos
+    build: {
+        outDir: 'dist', // Confirma que o build gera o diretório dist
+        assetsDir: 'assets'
+    }
 });
