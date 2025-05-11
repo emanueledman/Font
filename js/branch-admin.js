bd1c7db51400af36d66446ba4f9e03b389fa059e
Emanuel Joao <emanuelhedima@gmail.com>
2025-05-11 18:10:10 +0100

    dghfhj

--- js/branch-admin.js
+++ js/branch-admin.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = '/api/admin';
+    const API_BASE_URL = 'https://fila-facilita2-0-4uzw.onrender.com';
     const socket = io('/admin', { auth: { token: localStorage.getItem('authToken') } });
     let currentUser = null;
     let institutionId = null;
